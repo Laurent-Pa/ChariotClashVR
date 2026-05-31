4487ae9bbd9e4d27c1bb84ecbac02896e5f81b26
Nathan <nathanlgd14@gmail.com>
2026-05-31 11:26:30 +0200

    fix(player): adjust collision layer and camera transform for better VR interaction

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,6 +4,7 @@ var xr_interface: XRInterface
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
+	var camera := $Player/XRCamera3D
 
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialised succesfully")
@@ -11,7 +12,9 @@ func _ready():
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
 		get_viewport().use_xr = true
+		camera.transform.origin.y = 0.0
 	else:
 		print("OpenXR not initialised, please check if your headset is connected")
+		camera.transform.origin.y = 1.7
 	
 	$Player.add_to_group("player")
